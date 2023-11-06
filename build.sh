464a579d50bcbb9ac1dbc9fe7699e85a8ed4f731
Andy Meneely <andy.meneely@gmail.com>
2023-11-05 19:27:59 -0500

    quick and dirty build script

---
+++ build.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/Library/Application\ Support/typst/packages/local/exam/0.1.0
+cp lib.typ ~/Library/Application\ Support/typst/packages/local/exam/0.1.0
+cp typst.toml ~/Library/Application\ Support/typst/packages/local/exam/0.1.0
+cp LICENSE ~/Library/Application\ Support/typst/packages/local/exam/0.1.0
